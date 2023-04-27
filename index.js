d8a5ca6ad1c58433810703c68cdc5547d650c091
ltvinh <everwellmax@gmail.com>
2023-04-27 22:00:15 +0700

    modified

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function(){
+  scrollFunction();
+  fadeUp()
+}
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.backgroundColor = "#485461";
   } else {
     document.getElementById("navbar").style.backgroundColor = "transparent";
+  }
+}
+function fadeUp(){
+  var fadeUp = document.querySelectorAll(".fade-up");
+  console.log(fadeUp);
+  for(let i = 0; i < fadeUp.length; i++){
+    var windowHeight = window.innerHeight;
+    var elementTop = fadeUp[i].getBoundingClientRect().top;
+    var elementVisible = 500;
+    if(elementTop < windowHeight - elementVisible){
+      fadeUp[i].classList.add("animation-fade-up")
+    }
   }
 }
