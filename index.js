b81a9f0130d3ca4a9f526feb102d15788cd1e223
ltvinh <everwellmax@gmail.com>
2023-04-27 16:42:19 +0700

    modified

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.background = "#485461";
   } else {
-    document.getElementById("navbar").style.background = "none";
+    document.getElementById("navbar").style.background = "transparent";
   }
 }
