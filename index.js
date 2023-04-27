0fac88200d76769b249586e25244acc6f471fa06
ltvinh <everwellmax@gmail.com>
2023-04-27 16:43:35 +0700

    modified

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.background = "#485461";
+    document.getElementById("navbar").style.backgroundColor = "#485461";
   } else {
-    document.getElementById("navbar").style.background = "transparent";
+    document.getElementById("navbar").style.backgroundColor = "transparent";
   }
 }
